b5fd3e7f91f6221ff804687c9dd15e11f6b9c0d4
Francesco Caliumi <francesco.caliumi@gmail.com>
2015-01-05 21:47:57 +0100

    Added some setup and doc files fron Pylons/shootout example

---
+++ setup.py
@@ -0,0 +1,59 @@
+import os
+import sys
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+
+requires = [
+    'setuptools',
+    'pyramid>=1.3',
+    'SQLAlchemy',
+    'transaction',
+#    'pyramid_chameleon',
+    'pyramid_tm',
+    'pyramid_debugtoolbar',
+    'pyramid_exclog',
+    'zope.sqlalchemy',
+    'pyramid_simpleform',
+    'cryptacular',
+    'waitress',
+    'pycrypto',
+    'webtest',
+    ]
+
+if sys.version_info[:3] < (2,5,0):
+    raise RuntimeError('This application requires Python 2.6+')
+
+setup(name='shootout',
+      version='0.0.1',
+      description='Builder of Interfaces for RDF Data Authoring (BIRD-A)',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+        "Framework :: Pylons",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Internet :: WWW/HTTP :: WSGI",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author="Silvio Peroni, Francesco Caliumi",
+      author_email="silvio.peroni@unibo.it, francesco.caliumi@gmail.com",
+      url='...',
+      license="...",
+      keywords='birda rdf ontology',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      test_suite='bin.tests',
+      install_requires=requires,
+      entry_points="""\
+      [paste.app_factory]
+      main = bin:main
+      [console_scripts]
+      initialize_shootout_db = shootout.scripts.initializedb:main
+      """,
+      )
