752f97f93f3bb2cc748c15e565e542d2e22b946e
Francesco Caliumi <francesco.caliumi@gmail.com>
2015-01-18 15:54:52 +0100

    Minor changes

--- birda/models/__init__.py
+++ birda/models/__init__.py
@@ -8,22 +8,30 @@ import sqlalchemy.orm
 import zope.sqlalchemy
 import cryptacular.bcrypt
 
+# ------------------------------------------------------------------------------------------------ #
 
 # Allow "import birda.models.*"
 modules = glob.glob(os.path.dirname(__file__)+"/*.py")
 __all__ = [ os.path.basename(f)[:-3] for f in modules]
 
+# ------------------------------------------------------------------------------------------------ #
 
-DBSession = sqlalchemy.orm.scoped_session(sqlalchemy.orm.sessionmaker(extension=zope.sqlalchemy.ZopeTransactionExtension()))
+DBSession = sqlalchemy.orm.scoped_session(
+				sqlalchemy.orm.sessionmaker(
+					extension=zope.sqlalchemy.ZopeTransactionExtension()))
 
 Base = sqlalchemy.ext.declarative.declarative_base()
+
+def initialize_sql(engine):
+	DBSession.configure(bind=engine)
+	Base.metadata.bind = engine
+	Base.metadata.create_all(engine)
+
+# ------------------------------------------------------------------------------------------------ #
 
 crypt = cryptacular.bcrypt.BCRYPTPasswordManager()
 
 def hash_password(password):
 	return unicode(crypt.encode(password))
 
-def initialize_sql(engine):
-	DBSession.configure(bind=engine)
-	Base.metadata.bind = engine
-	Base.metadata.create_all(engine)
+# ------------------------------------------------------------------------------------------------ #
