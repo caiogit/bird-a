857f7c92bb5d1306f51fbf1007c11a40a7d3e847
Francesco Caliumi <francesco.caliumi@gmail.com>
2016-01-12 04:28:19 +0100

    - Refactoring

--- frontend/app/scripts/services/forms.js
+++ frontend/app/scripts/services/forms.js
@@ -5,66 +5,67 @@
  */
 
 angular.module('birdaApp')
-	.service('FormsService', function() {
+	.service('FormsService', ['ConfigService',
+		function(ConfigService) {
 
-		var self = this;
-		//var query = null;
-		var forms = null;
+			var self = this;
+			//var query = null;
+			var forms = null;
 
-		/* ----------------------------------------- */
+			/* ----------------------------------------- */
 
-		function init() {
-			self.retrieveForms();
-		}
+			function init() {
+				self.retrieveForms();
+			}
 
-		/* ========================================= */
+			/* ========================================= */
 
-		self.getForms = function() {
-			return forms;
-		};
+			self.getForms = function() {
+				return forms;
+			};
 
-		self.retrieveForms = function() {
-			forms = self.forms_Test1;
-		};
+			self.retrieveForms = function() {
+				forms = self.forms_Test1;
+			};
 
-		self.getFormByUri = function(formUri) {
-			var ret = null;
+			self.getFormByUri = function(formUri) {
+				var ret = null;
 
-			angular.forEach(forms.forms, function(form, key) {
-				if (formUri === form.uri) {
-					ret = form;
-				}
-			});
+				angular.forEach(forms.forms, function(form, key) {
+					if (formUri === form.uri) {
+						ret = form;
+					}
+				});
 
-			return ret;
-		};
+				return ret;
+			};
 
-		/* ========================================= */
+			/* ========================================= */
 
-		self.forms_Test1 = {
-			'forms': [
-				{
-					'uri': 'http://www.birda.it/form-person-1',
-					'type': 'http://xmlns.com/foaf/0.1/Person',
-					'label': 'FOAF Person Light',
-					'description': 'Form for editing idividuals in the FOAF Ontology.\nLight version.'
-				},
-				{
-					'uri': 'http://www.birda.it/form-person-2',
-					'type': 'http://xmlns.com/foaf/0.1/Person',
-					'label': 'FOAF Person Extended',
-					'description': 'Form for editing idividuals in the FOAF Ontology.\nExtended version'
-				},
-				{
-					'uri': 'http://www.birda.it/fuff',
-					'type': 'http://xmlns.com/foaf/0.1/Fuff',
-					'label': 'FUFF Object',
-					'description': 'Nothing much here'
-				}
-			]
-		};
+			self.forms_Test1 = {
+				'forms': [
+					{
+						'uri': 'http://www.birda.it/form-person-1',
+						'type': 'http://xmlns.com/foaf/0.1/Person',
+						'label': 'FOAF Person Light',
+						'description': 'Form for editing idividuals in the FOAF Ontology.\nLight version.'
+					},
+					{
+						'uri': 'http://www.birda.it/form-person-2',
+						'type': 'http://xmlns.com/foaf/0.1/Person',
+						'label': 'FOAF Person Extended',
+						'description': 'Form for editing idividuals in the FOAF Ontology.\nExtended version'
+					},
+					{
+						'uri': 'http://www.birda.it/fuff',
+						'type': 'http://xmlns.com/foaf/0.1/Fuff',
+						'label': 'FUFF Object',
+						'description': 'Nothing much here'
+					}
+				]
+			};
 
-		/* ========================================= */
+			/* ========================================= */
 
-		init();
-	});
+			init();
+		}]);
--- frontend/app/scripts/services/individuals-search.js
+++ frontend/app/scripts/services/individuals-search.js
@@ -5,251 +5,252 @@
  */
 
 angular.module('birdaApp')
-	.service('IndividualsSearchService', function() {
+	.service('IndividualsSearchService', ['ConfigService',
+		function(ConfigService) {
 
-		var self = this;
-		var params = null;
-		var results = null;
+			var self = this;
+			var params = null;
+			var results = null;
 
-		function init() {
-			self.clean();
-		}
+			function init() {
+				self.clean();
+			}
 
-		/* ========================================= */
+			/* ========================================= */
 
-		self.clean = function() {
-			params = {
-				'properties': [],
-				'filters': [],
-				'order-by': []
+			self.clean = function() {
+				params = {
+					'properties': [],
+					'filters': [],
+					'order-by': []
+				};
+				results = null;
 			};
-			results = null;
-		};
 
-		/* ----------------------------------------- */
+			/* ----------------------------------------- */
 
-		self.addProperty = function(property) {
-			if (results) {
-				console.log('IndividualsSearchService: Warning: altering params in a not cleaned status');
-			}
-			if ( (typeof property !== 'object') || (property === null)  ) {
-				throw 'IndividualsSearchService: "property" should be a not null object';
-			}
-			if ( ! ('uri' in property) ) {
-				throw 'IndividualsSearchService: "property" should have a "uri" property';
-			}
-
-			params.properties.push(property);
-		};
-
-		self.addFilter = function(filter) {
-			if (results) {
-				console.log('IndividualsSearchService: Warning: altering params in a not cleaned status');
-			}
-			if ( (typeof filter !== 'object') || (filter === null)  ) {
-				throw 'IndividualsSearchService: "filter" should be a not null object';
-			}
-			if ( ! ('property' in filter) ) {
-				throw 'IndividualsSearchService: "filter" should have a "property" property';
-			}
-			if ( ! ('value' in filter) ) {
-				throw 'IndividualsSearchService: "filter" should have a "value" property';
-			}
-			if ( ! ('match' in filter) ) {
-				throw 'IndividualsSearchService: "filter" should have a "match" property';
-			}
-
-			params.filters.push(filter);
-		};
-
-		self.addOrderBy = function(orderBy) {
-			if (results) {
-				console.log('IndividualsSearchService: Warning: altering params in a not cleaned status');
-			}
-			if ( (typeof orderBy !== 'object') || (orderBy === null)  ) {
-				throw 'IndividualsSearchService: "orderBy" should be a not null object';
-			}
-			if ( ! ('property' in orderBy) ) {
-				throw 'IndividualsSearchService: "orderBy" should have a "property" property';
-			}
-			if ( ! ('order' in orderBy) ) {
-				throw 'IndividualsSearchService: "orderBy" should have a "order" property';
-			}
-
-			params.order_by.push(orderBy);
-		};
-
-		/* ----------------------------------------- */
-
-		self.search = function() {
-
-			/** TODO: Remove debug code here **/
-
-			if ( (params.properties.length === 0) &&
-				 (params.filters[0].property === RDF_TYPE) &&
-				 (params.filters[0].value === 'http://xmlns.com/foaf/0.1/Person') ) {
-
-				results = self.test_personResults;
-				return;
-			}
-
-			if ( (params.properties.length === 0) &&
-				 (params.filters[0].property === RDF_TYPE) &&
-				 (params.filters[0].value === 'http://www.birda.it/fuff') ) {
-
-				results = self.test_fuffResults;
-				return;
-			}
-
-			throw "Azz";
-		};
-
-		self.getResults = function() {
-			return results;
-		};
-
-		/* ========================================= */
-
-		self.test_personResults = {
-			'individuals': [
-				{
-					'uri': 'http://ex.com/john-max-smith-01',
-					'type': 'http://xmlns.com/foaf/0.1/Person',
-					'label': 'John Max Smith',
-					'description': 'Famous actor'
-				},
-				{
-					'uri': 'http://ex.com/john-max-smith-02',
-					'type': 'http://xmlns.com/foaf/0.1/Person',
-					'label': 'John Max Smith',
-					'description': 'Famous actor'
-				},
-				{
-					'uri': 'http://ex.com/john-max-smith-03',
-					'type': 'http://xmlns.com/foaf/0.1/Person',
-					'label': 'John Max Smith',
-					'description': 'Famous actor'
-				},
-				{
-					'uri': 'http://ex.com/john-max-smith-04',
-					'type': 'http://xmlns.com/foaf/0.1/Person',
-					'label': 'John Max Smith',
-					'description': 'Famous actor'
-				},
-				{
-					'uri': 'http://ex.com/john-max-smith-05',
-					'type': 'http://xmlns.com/foaf/0.1/Person',
-					'label': 'John Max Smith',
-					'description': 'Famous actor'
-				},
-				{
-					'uri': 'http://ex.com/john-max-smith-06',
-					'type': 'http://xmlns.com/foaf/0.1/Person',
-					'label': 'John Max Smith',
-					'description': 'Famous actor'
-				},
-				{
-					'uri': 'http://ex.com/john-max-smith-07',
-					'type': 'http://xmlns.com/foaf/0.1/Person',
-					'label': 'John Max Smith',
-					'description': 'Famous actor'
-				},
-				{
-					'uri': 'http://ex.com/john-max-smith-08',
-					'type': 'http://xmlns.com/foaf/0.1/Person',
-					'label': 'John Max Smith',
-					'description': 'Famous actor'
-				},
-				{
-					'uri': 'http://ex.com/john-max-smith-09',
-					'type': 'http://xmlns.com/foaf/0.1/Person',
-					'label': 'John Max Smith',
-					'description': 'Famous actor'
-				},
-				{
-					'uri': 'http://ex.com/john-max-smith-10',
-					'type': 'http://xmlns.com/foaf/0.1/Person',
-					'label': 'John Max Smith',
-					'description': 'Famous actor'
-				},
-				{
-					'uri': 'http://ex.com/john-max-smith-11',
-					'type': 'http://xmlns.com/foaf/0.1/Person',
-					'label': 'John Max Smith',
-					'description': 'Famous actor'
-				},
-				{
-					'uri': 'http://ex.com/john-max-smith-12',
-					'type': 'http://xmlns.com/foaf/0.1/Person',
-					'label': 'John Max Smith',
-					'description': 'Famous actor'
-				},
-				{
-					'uri': 'http://ex.com/john-max-smith-13',
-					'type': 'http://xmlns.com/foaf/0.1/Person',
-					'label': 'John Max Smith',
-					'description': 'Famous actor'
-				},
-				{
-					'uri': 'http://ex.com/john-max-smith-14',
-					'type': 'http://xmlns.com/foaf/0.1/Person',
-					'label': 'John Max Smith',
-					'description': 'Famous actor'
-				},
-				{
-					'uri': 'http://ex.com/john-max-smith-15',
-					'type': 'http://xmlns.com/foaf/0.1/Person',
-					'label': 'John Max Smith',
-					'description': 'Famous actor'
-				},
-				{
-					'uri': 'http://ex.com/john-max-smith-16',
-					'type': 'http://xmlns.com/foaf/0.1/Person',
-					'label': 'John Max Smith',
-					'description': 'Famous actor'
-				},
-				{
-					'uri': 'http://ex.com/john-max-smith-17',
-					'type': 'http://xmlns.com/foaf/0.1/Person',
-					'label': 'John Max Smith',
-					'description': 'Famous actor'
-				},
-				{
-					'uri': 'http://ex.com/john-max-smith-18',
-					'type': 'http://xmlns.com/foaf/0.1/Person',
-					'label': 'John Max Smith',
-					'description': 'Famous actor'
-				},
-				{
-					'uri': 'http://ex.com/john-max-smith-19',
-					'type': 'http://xmlns.com/foaf/0.1/Person',
-					'label': 'John Max Smith',
-					'description': 'Famous actor'
+			self.addProperty = function(property) {
+				if (results) {
+					console.log('IndividualsSearchService: Warning: altering params in a not cleaned status');
 				}
-			]
-		};
-
-		self.test_fuffResults = {
-			'individuals': [
-				{
-					'uri': 'http://ex.com/1111',
-					'type': 'http://xmlns.com/foaf/0.1/Fuff',
-					'label': 'Pippo',
-					'Description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vitae nisl rutrum, placerat nisl ac.',
-					'properties': []
-				},
-				{
-					'uri': 'http://ex.com/2222',
-					'type': 'http://xmlns.com/foaf/0.1/Fuff',
-					'label': 'Pippo',
-					'Description': 'Suspendisse non sapien tempus, cursus nisi at, lacinia justo. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
-					'properties': []
+				if ( (typeof property !== 'object') || (property === null)  ) {
+					throw 'IndividualsSearchService: "property" should be a not null object';
 				}
-			]
-		};
+				if ( ! ('uri' in property) ) {
+					throw 'IndividualsSearchService: "property" should have a "uri" property';
+				}
 
-		/* ========================================= */
+				params.properties.push(property);
+			};
 
-		init();
+			self.addFilter = function(filter) {
+				if (results) {
+					console.log('IndividualsSearchService: Warning: altering params in a not cleaned status');
+				}
+				if ( (typeof filter !== 'object') || (filter === null)  ) {
+					throw 'IndividualsSearchService: "filter" should be a not null object';
+				}
+				if ( ! ('property' in filter) ) {
+					throw 'IndividualsSearchService: "filter" should have a "property" property';
+				}
+				if ( ! ('value' in filter) ) {
+					throw 'IndividualsSearchService: "filter" should have a "value" property';
+				}
+				if ( ! ('match' in filter) ) {
+					throw 'IndividualsSearchService: "filter" should have a "match" property';
+				}
 
-	});
+				params.filters.push(filter);
+			};
+
+			self.addOrderBy = function(orderBy) {
+				if (results) {
+					console.log('IndividualsSearchService: Warning: altering params in a not cleaned status');
+				}
+				if ( (typeof orderBy !== 'object') || (orderBy === null)  ) {
+					throw 'IndividualsSearchService: "orderBy" should be a not null object';
+				}
+				if ( ! ('property' in orderBy) ) {
+					throw 'IndividualsSearchService: "orderBy" should have a "property" property';
+				}
+				if ( ! ('order' in orderBy) ) {
+					throw 'IndividualsSearchService: "orderBy" should have a "order" property';
+				}
+
+				params.order_by.push(orderBy);
+			};
+
+			/* ----------------------------------------- */
+
+			self.search = function() {
+
+				/** TODO: Remove debug code here **/
+
+				if ( (params.properties.length === 0) &&
+					 (params.filters[0].property === RDF_TYPE) &&
+					 (params.filters[0].value === 'http://xmlns.com/foaf/0.1/Person') ) {
+
+					results = self.test_personResults;
+					return;
+				}
+
+				if ( (params.properties.length === 0) &&
+					 (params.filters[0].property === RDF_TYPE) &&
+					 (params.filters[0].value === 'http://www.birda.it/fuff') ) {
+
+					results = self.test_fuffResults;
+					return;
+				}
+
+				throw "Azz";
+			};
+
+			self.getResults = function() {
+				return results;
+			};
+
+			/* ========================================= */
+
+			self.test_personResults = {
+				'individuals': [
+					{
+						'uri': 'http://ex.com/john-max-smith-01',
+						'type': 'http://xmlns.com/foaf/0.1/Person',
+						'label': 'John Max Smith',
+						'description': 'Famous actor'
+					},
+					{
+						'uri': 'http://ex.com/john-max-smith-02',
+						'type': 'http://xmlns.com/foaf/0.1/Person',
+						'label': 'John Max Smith',
+						'description': 'Famous actor'
+					},
+					{
+						'uri': 'http://ex.com/john-max-smith-03',
+						'type': 'http://xmlns.com/foaf/0.1/Person',
+						'label': 'John Max Smith',
+						'description': 'Famous actor'
+					},
+					{
+						'uri': 'http://ex.com/john-max-smith-04',
+						'type': 'http://xmlns.com/foaf/0.1/Person',
+						'label': 'John Max Smith',
+						'description': 'Famous actor'
+					},
+					{
+						'uri': 'http://ex.com/john-max-smith-05',
+						'type': 'http://xmlns.com/foaf/0.1/Person',
+						'label': 'John Max Smith',
+						'description': 'Famous actor'
+					},
+					{
+						'uri': 'http://ex.com/john-max-smith-06',
+						'type': 'http://xmlns.com/foaf/0.1/Person',
+						'label': 'John Max Smith',
+						'description': 'Famous actor'
+					},
+					{
+						'uri': 'http://ex.com/john-max-smith-07',
+						'type': 'http://xmlns.com/foaf/0.1/Person',
+						'label': 'John Max Smith',
+						'description': 'Famous actor'
+					},
+					{
+						'uri': 'http://ex.com/john-max-smith-08',
+						'type': 'http://xmlns.com/foaf/0.1/Person',
+						'label': 'John Max Smith',
+						'description': 'Famous actor'
+					},
+					{
+						'uri': 'http://ex.com/john-max-smith-09',
+						'type': 'http://xmlns.com/foaf/0.1/Person',
+						'label': 'John Max Smith',
+						'description': 'Famous actor'
+					},
+					{
+						'uri': 'http://ex.com/john-max-smith-10',
+						'type': 'http://xmlns.com/foaf/0.1/Person',
+						'label': 'John Max Smith',
+						'description': 'Famous actor'
+					},
+					{
+						'uri': 'http://ex.com/john-max-smith-11',
+						'type': 'http://xmlns.com/foaf/0.1/Person',
+						'label': 'John Max Smith',
+						'description': 'Famous actor'
+					},
+					{
+						'uri': 'http://ex.com/john-max-smith-12',
+						'type': 'http://xmlns.com/foaf/0.1/Person',
+						'label': 'John Max Smith',
+						'description': 'Famous actor'
+					},
+					{
+						'uri': 'http://ex.com/john-max-smith-13',
+						'type': 'http://xmlns.com/foaf/0.1/Person',
+						'label': 'John Max Smith',
+						'description': 'Famous actor'
+					},
+					{
+						'uri': 'http://ex.com/john-max-smith-14',
+						'type': 'http://xmlns.com/foaf/0.1/Person',
+						'label': 'John Max Smith',
+						'description': 'Famous actor'
+					},
+					{
+						'uri': 'http://ex.com/john-max-smith-15',
+						'type': 'http://xmlns.com/foaf/0.1/Person',
+						'label': 'John Max Smith',
+						'description': 'Famous actor'
+					},
+					{
+						'uri': 'http://ex.com/john-max-smith-16',
+						'type': 'http://xmlns.com/foaf/0.1/Person',
+						'label': 'John Max Smith',
+						'description': 'Famous actor'
+					},
+					{
+						'uri': 'http://ex.com/john-max-smith-17',
+						'type': 'http://xmlns.com/foaf/0.1/Person',
+						'label': 'John Max Smith',
+						'description': 'Famous actor'
+					},
+					{
+						'uri': 'http://ex.com/john-max-smith-18',
+						'type': 'http://xmlns.com/foaf/0.1/Person',
+						'label': 'John Max Smith',
+						'description': 'Famous actor'
+					},
+					{
+						'uri': 'http://ex.com/john-max-smith-19',
+						'type': 'http://xmlns.com/foaf/0.1/Person',
+						'label': 'John Max Smith',
+						'description': 'Famous actor'
+					}
+				]
+			};
+
+			self.test_fuffResults = {
+				'individuals': [
+					{
+						'uri': 'http://ex.com/1111',
+						'type': 'http://xmlns.com/foaf/0.1/Fuff',
+						'label': 'Pippo',
+						'Description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vitae nisl rutrum, placerat nisl ac.',
+						'properties': []
+					},
+					{
+						'uri': 'http://ex.com/2222',
+						'type': 'http://xmlns.com/foaf/0.1/Fuff',
+						'label': 'Pippo',
+						'Description': 'Suspendisse non sapien tempus, cursus nisi at, lacinia justo. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
+						'properties': []
+					}
+				]
+			};
+
+			/* ========================================= */
+
+			init();
+
+		}]);
