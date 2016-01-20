b4b0c13cd821545b3faf7ad6e32f709696c059dc
Francesco Caliumi <francesco.caliumi@gmail.com>
2016-01-20 07:34:24 +0100

    - Backend: Storage: WIP

--- backend/birda/storage/file_storage.py
+++ backend/birda/storage/file_storage.py
@@ -6,79 +6,42 @@ import rdflib
 import storage
 import pprint
 
-# State if a birda instances without a corresponding repo instances will create an empty repo
-CREATE_EMPTY_REPOS = True
+# ============================================================================ #
 
-class FileStorage(storage.Storage):
-	"""
-	Storage based on files
-	"""
+class FileConnection(object):
 
-	# Dictionary "ontology_id" -> dictionary with keys:
-	#	'birda': birda instance file
-	#   'repos': ontology instances file
-	ontologies = {}
+	def __init__(self, settings, dataset='', namespaces={}):
+		pass
 
-	# ----------------------------------------------------------------------- #
+	def sparql_query(self, query):
+		"""
+		Exectutes a read-only sparql query
 
-	def __init__(self, prj_path):
-		self.prj_path = prj_path
-		self.catalog_path = os.path.abspath( self.prj_path + os.path.sep + 'catalog' )
-		self.birda_catalog_path = self.catalog_path + os.sep + 'birda'
-		self.repos_catalog_path = self.catalog_path + os.sep + 'ontologies'
+		:return: Result object
+		"""
 
-		# Initialize catalog
-		self.reload_available_ontologies_catalog()
+		raise NotImplementedError("This method should be implemented by subclasses")
 
 	# ----------------------------------------------------------------------- #
 
-	def reload_available_ontologies_catalog(self):
+	def sparql_update(self, query):
+		"""
+		Exectutes a write-only sparql query
 
-		def get_basename(file_path):
-			return os.path.splitext( os.path.basename(file_path) )[0]
+		:return: ???
+		"""
 
-		def get_extension(file_path):
-			return os.path.splitext( os.path.basename(file_path) )[1]
-
-		self.ontologies = {}
-
-		birda_files = os.listdir(self.birda_catalog_path)
-		repos_files = os.listdir(self.repos_catalog_path)
-		for birda_file in birda_files:
-			onto_id = get_basename(birda_file)
-
-			repos_file = ''
-			for r_file in repos_files:
-				if get_basename(r_file) == onto_id:
-					repos_file = r_file
-					break
-
-			if CREATE_EMPTY_REPOS:
-				if not repos_file:
-					repos_file = onto_id + get_extension(birda_file)
-					repos_file_path = self.catalog_path + os.sep + 'ontologies' + os.sep + repos_file
-					print "Creating %(repos_file_path)s" % vars()
-					f = open(repos_file_path,'w')
-					f.close()
-
-			if repos_file:
-				self.ontologies[onto_id] = {
-					'birda': self.catalog_path + os.sep + 'birda' + os.sep + birda_file,
-					'repos': self.catalog_path + os.sep + 'ontologies' + os.sep + repos_file,
-				}
-
-		print pprint.pformat( self.ontologies )
+		raise NotImplementedError("This method should be implemented by subclasses")
 
 	# ----------------------------------------------------------------------- #
 
-	def get_birda_instances(self):
-		instances = {}
+	def close(self):
+		"""
+		Close the connection
 
-		for onto_id in self.ontologies.keys():
-			rdf = rdflib.Graph()
-			instances[onto_id] = rdf.load(self.ontologies[onto_id]['birda'])
-
-		return instances
+		:return: None
+		"""
+		raise NotImplementedError("This method should be implemented by subclasses")
 
 # ================================================================================================ #
 
--- backend/birda/storage/storage.py
+++ backend/birda/storage/storage.py
@@ -1,6 +1,123 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# References:
+# - Static and abstract methods: https://julien.danjou.info/blog/2013/guide-python-static-class-abstract-methods
+# - Singletons in Python: http://stackoverflow.com/questions/6760685/creating-a-singleton-in-python?rq=1
+# - Lock acquisition with a decorator: http://stackoverflow.com/questions/489720/what-are-some-common-uses-for-python-decorators/490090#490090
+# - Python thread synchronization guide: http://www.laurentluce.com/posts/python-threads-synchronization-locks-rlocks-semaphores-conditions-events-and-queues/
+
+import rdflib
+import abc
+
+NAMESPACES = {
+	'rdf': rdflib.namespace.RDF,
+	'rdfs': rdflib.namespace.RDFS,
+	'xsd': rdflib.namespace.XSD,
+	'foaf': rdflib.namespace.FOAF,
+	'skos': rdflib.namespace.SKOS,
+	'co': rdflib.Namespace("http://purl.org/co/"),
+	'birda': rdflib.Namespace("http://w3id.org/ontologies/bird-a/"),
+	'binst': rdflib.Namespace("http://pippo.it/birda-data/"),
+	'tinst': rdflib.Namespace("http://pippo.it/target-data/")
+}
+
+# ============================================================================ #
+
+class Results(object):
+	"""
+	Wrapper for sparql_results who provides some utility features
+	"""
+
+	query = ""
+	sparql_results = []
+
+	# ----------------------------------------------------------------------- #
+
+	def __init__(self, query, sparql_results):
+		self.query = query
+		self.sparql_results = sparql_results
+
+	# ----------------------------------------------------------------------- #
+
+	def getDictList(self):
+		"""
+		Get a list of dictionaries which keys are strings and values are
+		RDFLib object
+
+		:return: List of dictionaries
+		"""
+
+		return self.sparql_results.bindings
+
+	# ----------------------------------------------------------------------- #
+
+	def getSimpleDictList(self):
+		"""
+		Get a list of dictionaries which keys are strings and values are
+		strings, ints and dates
+
+		:return: List of dictionaries
+		"""
+
+		# TODO
+		pass
+
+	# ----------------------------------------------------------------------- #
+
+	def printQueryResults(self):
+		"""
+		Print query results in a MySQL ascii tab fashion
+
+		:return: None
+		"""
+
+		# TODO
+
+# ============================================================================ #
+
+class Connection(object):
+	__metaclass__  = abc.ABCMeta
+
+	@abc.abstractmethod
+	def __init__(self, settings, dataset='', namespaces={}):
+		pass
+
+	@abc.abstractmethod
+	def sparql_query(self, query):
+		"""
+		Exectutes a read-only sparql query
+
+		:return: Result object
+		"""
+
+		raise NotImplementedError("This method should be implemented by subclasses")
+
+	# ----------------------------------------------------------------------- #
+
+	@abc.abstractmethod
+	def sparql_update(self, query):
+		"""
+		Exectutes a write-only sparql query
+
+		:return: ???
+		"""
+
+		raise NotImplementedError("This method should be implemented by subclasses")
+
+	# ----------------------------------------------------------------------- #
+
+	@abc.abstractmethod
+	def close(self):
+		"""
+		Close the connection
+
+		:return: None
+		"""
+		raise NotImplementedError("This method should be implemented by subclasses")
+
+# ============================================================================ #
+
 class Storage(object):
 	"""
 	Storage abstract class
@@ -14,51 +131,17 @@ class Storage(object):
 
 	# ----------------------------------------------------------------------- #
 
-	def reload_available_binst_catalog(self):
+	@staticmethod
+	def connect(settings, dataset='', namespaces=NAMESPACES):
 		"""
-		Reload the internal bird-a instances catalog
+		Creates a connection to a sparql endpoint using "setting" parameters
 
-		:return: None
-		"""
-
-		raise NotImplementedError("This method should be implemented by subclasses")
-
-	# ----------------------------------------------------------------------- #
-
-	def get_binst_instances(self, binst_id):
-		"""
-		Get a list of all instances of a given bird-a instance
-
-		:param binst_id: bird-a instance id
-		:return: List of dictionaries (keys: 'id', 'title')
+		:return: Connection object (sublass of storage.Connection)
 		"""
 
-		raise NotImplementedError("This method should be implemented by subclasses")
-
-	# ----------------------------------------------------------------------- #
-
-	def get_binst_iri(self, binst_id):
-		"""
-		Get the bird-a instance IRI
-
-		:param binst_id: (string) bird-a instance id
-		:return: (string) IRI of the ontology
-		"""
-
-		raise NotImplementedError("This method should be implemented by subclasses")
-
-	# ----------------------------------------------------------------------- #
-
-	def get_all_binst(self):
-		"""
-		Return all bird-a instances in rdf format
-
-		:return: Dictionary {"<binst_id>" -> rdflib.Graph}
-		"""
 		raise NotImplementedError("This method should be implemented by subclasses")
 
 # ================================================================================================ #
 
 if __name__ == '__main__':
 	storage = Storage()
-	storage.get_ontologies()
